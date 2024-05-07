dc71d607f24eb6f7023f64b824a3af40222c1a1a
MJMMB <mjmmbandeira@gmail.com>
2024-05-06 23:23:11 -0300

    my first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+let key = "cebcd482eda57fa9a6714c1c2ba91885"
+
+
+async function buscarcidade(cidade) {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric",).then(resposta => resposta.json())
+    console.log(dados)
+    tela(dados)
+}
+
+function clicarnobotao() {
+    let cidade = document.querySelector(".cidade").value
+
+    buscarcidade(cidade)
+
+}
+
+function tela(dados) {
+    document.querySelector(".tempo").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector('.icone').src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".Humidade").innerHTML = "Humidade: " + dados.main.humidity + "%"
+      
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    let mensagem = document.getElementById("mensagem");
+  
+    if (typeof dados.message === "undefined") {
+      mensagem.classList.remove("hidden");
+      setTimeout(function() {
+        mensagem.classList.add("fade-out");
+      }, 2000);
+    }
+    });
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
